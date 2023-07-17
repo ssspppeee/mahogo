ae4971cbe8e492d5ec74c4fde7fdf0333fb582eb
Samuel Esler <sam.p.esler@gmail.com>
2023-07-17 22:26:04 +1000

    Increase node pool to 2 to accomodate NGINX reverse proxy

--- infra/main.tf
+++ infra/main.tf
@@ -34,7 +34,7 @@ resource "digitalocean_kubernetes_cluster" "mahogo_cluster" {
   node_pool {
     name       = "unipool"
     size       = "s-1vcpu-2gb"
-    node_count = 1
+    node_count = 2
   }
 }
 
